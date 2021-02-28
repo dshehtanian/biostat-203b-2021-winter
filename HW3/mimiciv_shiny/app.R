0caf8075d070533f0e548b81679bff3f21d7549b
dshehtanian <dshehtanian@gmail.com>
2021-02-28 22:42:35 +0000

    Rmd file knits properly and Shiny App can display all chart and lab data.

--- HW3/mimiciv_shiny/app.R
+++ HW3/mimiciv_shiny/app.R
@@ -8,51 +8,98 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(ggplot2)
 
 icudata <- readRDS("./icu_cohort.rds")
-    
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("ICU Cohort Data Summmary"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("var", 
-                        label = "Choose a variable to display",
-                        choices = c("Heart Rate", "Respiratory Rate"),
-                        selected = "Heart Rate"),
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 100,
-                        value = 30)
   
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  # Application title
+  titlePanel("ICU Cohort Data Summmary"),
+  
+  selectInput("gtype",
+              label = "How do you want to display the data?",
+              choices = c("Histogram", "Boxplot"), 
+              selected = "Histogram"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Heart Rate", 
+                              "Non-Invasive Blood Pressure - Systolic",
+                              "Non-Invasive Blood Pressure - Mean",
+                              "Respiratory Rate", "Temperature (F)",
+                              "Arterial Blood Pressure - Systolic",
+                              "Arterial Blood Pressure - Mean",
+                              "Bicarbonate", "Calcium", "Chloride",
+                              "Creatinine", "Glucose", "Magnesium",
+                              "Potassium", "Sodium", "Hematocrit", 
+                              "White Blood Cell", "Lactate"),
+                  selected = "Heart Rate"),
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 200,
+                  value = 50)
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        data1 <- switch(input$var, 
-                       "Heart Rate" = icudata$heart_rate,
-                       "Respiratory Rate" = icudata$respiratory_rate
-                       )
-        x <- data1
-        bins <- 100
-        # draw the histogram with the specified number of bins
-        hist(data1, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    data1 <- switch(input$var, 
+                    "Heart Rate" = icudata$heart_rate,
+                    "Non-Invasive Blood Pressure - Systolic" = 
+                      icudata$non_invasive_blood_pressure_systolic,
+                    "Non-Invasive Blood Pressure - Mean" = 
+                      icudata$non_invasive_blood_pressure_mean,
+                    "Respiratory Rate" = icudata$respiratory_rate,
+                    "Temperature (F)" = icudata$temperature_fahrenheit,
+                    "Arterial Blood Pressure - Systolic" = 
+                      icudata$arterial_blood_pressure_systolic,
+                    "Arterial Blood Pressure - Mean" = 
+                      icudata$arterial_blood_pressure_mean,
+                    "Bicarbonate" = icudata$bicarbonate, 
+                    "Calcium" = icudata$calcium, 
+                    "Chloride" = icudata$chloride,
+                    "Creatinine" = icudata$creatinine, 
+                    "Glucose" = icudata$glucose, 
+                    "Magnesium" = icudata$magnesium,
+                    "Potassium" = icudata$potassium, 
+                    "Sodium" = icudata$sodium, 
+                    "Hematocrit" = icudata$hematocrit, 
+                    "White Blood Cell" = icudata$wbc,
+                    "Lactate" = icudata$lactate
+    )
+    x <- data.frame(data1)
+    names(x) <- c("obs_val")
+    bins <- seq(min(data1, na.rm = TRUE),max(data1, na.rm = TRUE), length.out = input$bins+1)
+    # draw the histogram with the specified number of bins
+    if ("Histogram" %in% input$gtype) {
+      hist(data1, breaks = bins, col = 'steelblue', border = 'white', 
+           xlab = str_c(input$var))
+    }
+    if ("Boxplot" %in% input$gtype) {
+      x %>% ggplot(aes(x = obs_val)) + geom_boxplot() + 
+        labs(x = str_c(input$var)) 
+    }
+    
+  })
+  
+  
 }
 
 # Run the application 
